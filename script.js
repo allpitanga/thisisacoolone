f8c6b927efe757cc959dbdb1906517a7d40f0200
Allan <allan9pires@gmail.com>
2025-11-09 17:09:55 -0300

    INIT

---
+++ script.js
@@ -0,0 +1,7 @@
+function alertLove() {
+    alert("ALERT: i love you and i am always rooting for your success <3");
+}
+
+const btn = document.querySelector("#button");
+
+btn.addEventListener("click", alertLove);
